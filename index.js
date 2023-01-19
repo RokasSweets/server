fcf0365b266615a7ce6eb26fd4de2c3a892ce08c
RokasSweets <sweetsound741@gmail.com>
2023-01-19 13:44:30 +0200

    app init

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+import { server } from "./lib/server.js";
+
 const app = {};
 
-//paleidinejam projekta
 app.init = () => {
-    //trukstamu pradinio turinio generavimas:
-    //--folder
-    //--file
-    //gauti prisijungima prie db
-    //uzkurti serveri (DBconnection)
+    // trukstamu pradinio turinio generavimas:
+    // - folder
+    // - file
+    // gauti prisijungima prie DB
+    // uzkurti serveri
+    server.init();
+
+    // const timer = setInterval(() => {
+    //     console.log('apsivalymas....');
+    //     // reguliarus procesai:
+    //     // - info sinchronizavimas/atsinaujinimas
+    //     // - failu archyvavimas
+    //     // - info agregravimas (statistika)
+    //     // - nereikalingos info salinimas:
+    //     //      - failu trinimas
+    //     //      - DB optimizavimas
+    // }, 5000);
 }
 
 app.init();
