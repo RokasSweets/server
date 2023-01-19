f89a7693a27e5dc8407a80cddab0a4dcf91299e1
RokasSweets <sweetsound741@gmail.com>
2023-01-19 13:10:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = {};
+
+//paleidinejam projekta
+app.init = () => {
+    //trukstamu pradinio turinio generavimas:
+    //--folder
+    //--file
+    //gauti prisijungima prie db
+    //uzkurti serveri (DBconnection)
+}
+
+app.init();
+
+export { app }
