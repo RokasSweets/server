340e16b129f3317963d8eaac04c331d8df7d11ac
RokasSweets <sweetsound741@gmail.com>
2023-01-26 13:28:07 +0200

    Mime tyoes

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ const config = {};
 config.dev = {
     name: 'dev',
     port: 41069,
+    cache: 0,
 }
 
 config.prod = {
     name: 'prod',
     port: 41070,
+    cache: 3600,
 }
 
 const envName = process.env.NODE_ENV;
